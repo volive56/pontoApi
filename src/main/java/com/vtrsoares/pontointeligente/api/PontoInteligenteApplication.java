d07bfb43ded7df2b68f102fca5b4c866ae749a9b
vitor.oliveira <vitor.oliveira@objective.com>
2022-02-23 11:48:02 -0300

    commit test

--- src/main/java/com/vtrsoares/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/vtrsoares/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,12 +2,13 @@ package com.vtrsoares.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
+		System.out.println("passou");
 	}
-
 }
